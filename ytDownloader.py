8724589959e5f0de3f4c16b7a558990b9c46cb29
Jorge <75973913+Vangdale@users.noreply.github.com>
2022-09-09 17:22:21 +0200

    version  1.0.0

---
+++ ytDownloader.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from pytube import YouTube
+
+st.title("Youtube Video Donwloader") 
+st.subheader("Enter the URL:") 
+url = st.text_input(label='URL')
+
+if url != '':
+    yt = YouTube(url)
+    st.image(yt.thumbnail_url, width=300)
+    st.subheader('''
+    {}
+    ## Length: {} seconds
+    ## Rating: {} 
+    '''.format(yt.title , yt.length , yt.rating))
+    video = yt.streams
+    if len(video) > 0:
+        downloaded , download_audio = False , False
+        download_video = st.button("Download Video")
+        if yt.streams.filter(only_audio=True):
+            download_audio = st.button("Download Audio Only")
+        if download_video:
+            
+            video.get_highest_resolution().download()
+            downloaded = True
+        if download_audio:
+            video.filter(only_audio=True).last().download()
+            downloaded = True
+        if downloaded:
+            st.subheader("Download Complete")
+    else:
+        st.subheader("Sorry, this video can not be downloaded")
